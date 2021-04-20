41802cbba450a3be6aa92510d98acfbd45475ace
aindriu80 <aindriu80@gmail.com>
2021-04-20 15:58:34 +0100

    Mouse Events & Drawing a line

--- resources/script.js
+++ resources/script.js
@@ -2,10 +2,36 @@ const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
 let size = 20
+let isPressed = false
 let color = 'black'
 let x
 let y
 
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true
+
+  x = e.offsetX
+  y = e.offsetY
+})
+
+canvas.addEventListener('mouseup', (e) => {
+  isPressed = false
+
+  x = undefined
+  y = undefined
+})
+
+canvas.addEventListener('mousemove', (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX
+    const y2 = e.offsetY
+    drawCircle(x2, y2)
+    drawLine(x, y, x2, y2)
+
+    x = x2
+    y = y2
+  }
+})
 function drawCircle(x, y) {
   ctx.beginPath()
   ctx.arc(x, y, size, 0, Math.PI * 2)
@@ -18,10 +44,7 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1)
   ctx.lineTo(x2, y2)
   ctx.strokeStyle = color
-  ctx.lineWidth = size
+  ctx.lineWidth = size * 2
   ctx.stroke()
   console.log('this')
 }
-
-drawCircle(100, 100)
-drawLine(300, 300, 200, 500)
